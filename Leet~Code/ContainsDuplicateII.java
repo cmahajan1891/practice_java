2347c20abacc20585187472584226ac17b6006ed
cmahajan1891 <chetan_mahajan@gwu.edu>
2017-09-17 13:50:57 -0400

    Merge Changes for efficient way to solve the problem.

--- Leet~Code/ContainsDuplicateII.java
+++ Leet~Code/ContainsDuplicateII.java
@@ -1,6 +1,9 @@
 import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
 
 class ContainsDuplicateII {
+
    boolean hasDuplicate(int nums[], int k) {
 
     int min = Integer.MAX_VALUE;
@@ -20,8 +23,24 @@ class ContainsDuplicateII {
     return false;
   }
 
+  boolean calculateNearbyDuplicate(int []nums, int k){
+    //HashMap<Integer, Integer> map = new HashMap<>();
+    int j = 0;
+    HashSet<Integer> set = new HashSet();
+    for (int i = 0; i < nums.length ; i++) {
+      if (!set.add(nums[i])) {
+        return true;
+      }
+      if (set.size()>=k) {
+        set.remove(nums[j++]);
+      }
+    }
+    return false;
+  }
+
   public static void main(String[] args) {
     ContainsDuplicateII c = new ContainsDuplicateII();
-    System.out.println(c.hasDuplicate(new int []{8, 94, 3, 5 ,24, 8}, 4));
+    System.out.println(c.hasDuplicate(new int []{8, 94, 3, 5 ,24, 5}, 4));
+    System.out.println(c.calculateNearbyDuplicate(new int []{8, 94, 3, 5 ,24, 5}, 4));
   }
 }
